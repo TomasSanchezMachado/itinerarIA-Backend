f9ead247a42ac3f6a4036e67d8aac89ffcc5db0f
Tomas <sanchezmachadotomas@gmail.com>
2024-09-10 16:33:09 -0300

    Actualizacion esquema de usuario

--- dist/schemas/usuario.js
+++ dist/schemas/usuario.js
@@ -11,11 +11,11 @@ export const putSchema = z.object({
     nombres: z.string({
         invalid_type_error: 'El nombre debe ser un string',
         required_error: 'El nombre es requerido',
-    }).min(3).regex(/^[A-Za-z ]+$/, 'El nombre debe contener solo letras'),
+    }).min(3, 'El nombre debe tener al menos 3 caracteres').max(15, 'El nombre puede tener como maximo 25 caracteres').regex(/^[A-Za-z ]+$/, 'El nombre debe contener solo letras'),
     apellidos: z.string({
         invalid_type_error: 'El apellido debe ser un string',
         required_error: 'El apellido es requerido',
-    }).min(3).regex(/^[A-Za-z ]+$/, 'El apellido debe contener solo letras'),
+    }).min(3, 'El apellido debe tener al menos 3 caracteres').max(25, 'El apellido puede tener como maximo 25 caracteres').regex(/^[A-Za-z ]+$/, 'El apellido debe contener solo letras'),
     mail: z.string({
         invalid_type_error: 'El mail debe ser un string',
         required_error: 'El mail es requerido',
@@ -23,26 +23,26 @@ export const putSchema = z.object({
     nroTelefono: z.string({
         invalid_type_error: 'El número de teléfono debe ser un string',
         required_error: 'El número de teléfono es requerido',
-    }).min(7),
+    }).min(7, 'El numero de telefono debe tener al menos 7 digitos').max(14, 'El numero de telefono puede tener como maximo 14 digitos').regex(/[0-9]/),
 });
 export const patchSchema = z.object({
     username: z.string({
         invalid_type_error: 'El nombre de usuario debe ser un string'
-    }).regex(/^[a-zA-Z0-9_-]{3,30}$/, 'El nombre de usuario debe tener entre 3 y 30 caracteres y solo puede contener letras, números, guiones bajos y guiones medios').optional(),
+    }).min(3, 'El nombre de usuario debe tener al menos 3 caracteres').max(15, 'El nombre de usuario puede tener como maximo 15 caracteres').regex(/^[a-zA-Z0-9_-]{3,30}$/, 'El nombre de usuario debe tener entre 3 y 30 caracteres y solo puede contener letras, números, guiones bajos y guiones medios').optional(),
     password: z.string({
         invalid_type_error: 'La contraseña debe ser un string'
     }).regex(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/, 'La contraseña debe tener al menos 8 caracteres, una letra mayúscula y un número').optional(),
     nombres: z.string({
         invalid_type_error: 'El nombre debe ser un string'
-    }).min(3).regex(/^[A-Za-z ]+$/, 'El nombre debe contener solo letras').optional(),
+    }).min(3, 'El nombre debe tener al menos 3 caracteres').max(15, 'El nombre puede tener como maximo 25 caracteres').regex(/^[A-Za-z ]+$/, 'El nombre debe contener solo letras').optional(),
     apellidos: z.string({
         invalid_type_error: 'El apellido debe ser un string'
-    }).min(3).regex(/^[A-Za-z ]+$/, 'El apellido debe contener solo letras').optional(),
+    }).min(3, 'El apellido debe tener al menos 3 caracteres').max(25, 'El apellido puede tener como maximo 25 caracteres').regex(/^[A-Za-z ]+$/, 'El apellido debe contener solo letras').optional(),
     mail: z.string({
         invalid_type_error: 'El mail debe ser un string'
     }).email('El mail debe tener un formato válido').optional(),
     nroTelefono: z.string({
         invalid_type_error: 'El número de teléfono debe ser un string'
-    }).min(7).optional(),
+    }).min(7, 'El numero de telefono debe tener al menos 7 digitos').max(14, 'El numero de telefono puede tener como maximo 14 digitos').regex(/[0-9]/).optional()
 });
 //# sourceMappingURL=usuario.js.map

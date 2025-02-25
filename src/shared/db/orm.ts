f829b610eb569d81edee76c0717d13efcbd4b6b5
Nicolás Escobar <nicoescobar666@gmail.com>
2025-02-25 15:24:15 -0300

    Configuración para crear BD de testing
    añadí un script para correr el backend en modo testing, de modo que se utilice una BD de prueba distinta a la de producción.

--- src/shared/db/orm.ts
+++ src/shared/db/orm.ts
@@ -4,10 +4,17 @@ import { MongoHighlighter } from "@mikro-orm/mongo-highlighter";
 const mongoUsername = process.env.MONGO_INITDB_ROOT_USERNAME || "root";
 const mongoPassword = process.env.MONGO_INITDB_ROOT_PASSWORD || "example";
 
+const dbName =
+  process.env.NODE_ENV === "test" ? "itinerarIA_test" : "itinerarIA";
+
+if(process.env.NODE_ENV === "test"){
+  console.log("Using test database,", dbName);
+}
+
 export const orm = await MikroORM.init({
   entities: ["./dist/**/*.entity.js"],
   entitiesTs: ["./src/**/*.entity.ts"],
-  dbName: "itinerarIA",
+  dbName: dbName, // Usa la BD de testing si estamos en test
   type: "mongo",
   clientUrl: `mongodb://localhost:27017`,
   //cambiar a la linea de abajo para usar docker compose

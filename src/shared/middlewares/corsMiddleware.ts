2794103c2a5c948255ab0250d7a7108a136f8ea1
Vittorio Maragliano <maraglianovittorio@gmail.com>
2025-04-17 14:11:00 -0300

    actualizo ruta

--- src/shared/middlewares/corsMiddleware.ts
+++ src/shared/middlewares/corsMiddleware.ts
@@ -5,7 +5,7 @@ const ACCEPTED_ORIGINS = [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://itineraria-backend-production.up.railway.app/",
-  "https://itinerariafrontend.vercel.app/",
+  "https://itinerariafrontend.vercel.app",
 ];
 
 interface CorsMiddlewareOptions {

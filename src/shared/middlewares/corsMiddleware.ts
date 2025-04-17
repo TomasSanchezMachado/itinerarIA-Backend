3b933624153528011ad9166cb53a7164da1ec511
Vittorio Maragliano <maraglianovittorio@gmail.com>
2025-04-16 21:32:54 -0300

    Agrego ruta de railway

--- src/shared/middlewares/corsMiddleware.ts
+++ src/shared/middlewares/corsMiddleware.ts
@@ -5,6 +5,7 @@ const ACCEPTED_ORIGINS = [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://itineraria-backend.onrender.com",
+  "itineraria-backend-production.up.railway.app",
 ];
 
 interface CorsMiddlewareOptions {
